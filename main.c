4bd57c00e31199ebc7aa6759fc064bece51ab6ac
ithecontributor <andres.narva@gmail.com>
2017-01-24 11:11:10 -0800

    Update main.c

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ struct group_info *groups_alloc(int gidsetsize){
 
     group_info = kmalloc(sizeof(*group_info) + nblocks*sizeof(gid_t *), GFP_USER);
 
-    if (!group_info)
+ (!group_info)
 
         return NULL;
 
@@ -56,7 +56,7 @@ struct group_info *groups_alloc(int gidsetsize){
 
 
 
-    if (grp != cred->egid)
+ (grp != cred->egid)
 
         retval = groups_search(cred->group_info, grp);
 
@@ -72,7 +72,7 @@ struct group_info *groups_alloc(int gidsetsize){
 
 out_undo_partial_alloc:
 
-    while (--i >= 0) {
+(--i >= 0) {
 
         free_page((unsigned long)group_info->blocks[i]);
 
@@ -98,7 +98,7 @@ void groups_free(struct group_info *group_info)
 
         int i;
 
-        for (i = 0; i < group_info->nblocks; i++)
+ (i = 0; i < group_info->nblocks; i++)
 
             free_page((unsigned long)group_info->blocks[i]);
 
@@ -122,7 +122,7 @@ static int groups_to_user(gid_t __user *grouplist,
 
 {
 
-    int i;
+int i;
 
     unsigned int count = group_info->ngroups;
 
@@ -130,13 +130,13 @@ static int groups_to_user(gid_t __user *grouplist,
 
     for (i = 0; i < group_info->nblocks; i++) {
 
-        unsigned int cp_count = min(NGROUPS_PER_BLOCK, count);
+unsigned int cp_count = min(NGROUPS_PER_BLOCK, count);
 
         unsigned int len = cp_count * sizeof(*grouplist);
 
 
 
-        if (copy_to_user(grouplist, group_info->blocks[i], len))
+(copy_to_user(grouplist, group_info->blocks[i], len))
 
             return -EFAULT;
 
@@ -158,7 +158,7 @@ static int groups_to_user(gid_t __user *grouplist,
 
 static int groups_from_user(struct group_info *group_info,
 
-    gid_t __user *grouplist)
+gid_t __user *grouplist)
 
 {
 
