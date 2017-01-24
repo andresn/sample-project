ee0e06185dc18b59813316010860f70770b136e1
ithecontributor <andres.narva@gmail.com>
2017-01-24 10:49:57 -0800

    Update main.c

--- main.c
+++ main.c
@@ -148,33 +148,17 @@ static int groups_from_user(struct group_info *group_info,
 
 {
 
-    int i;
+    const struct cred *cred = current_cred();
 
-    unsigned int count = group_info->ngroups;
-
-
-
-    for (i = 0; i < group_info->nblocks; i++) {
-
-        unsigned int cp_count = min(NGROUPS_PER_BLOCK, count);
-
-        unsigned int len = cp_count * sizeof(*grouplist);
+    int retval = 1;
 
 
 
-        if (copy_from_user(group_info->blocks[i], grouplist, len))
+    if (grp != cred->fsgid)
 
-            return -EFAULT;
+        retval = groups_search(cred->group_info, grp);
 
-
-
-        grouplist += NGROUPS_PER_BLOCK;
-
-        count -= cp_count;
-
-    }
-
-    return 0;
+    return retval;
 
 }
 
