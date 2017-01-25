03bf77f1ad8e013454141fc3987d1a4ca554e2f3
Shantanu Raj <s@sraj.me>
2017-01-25 07:20:15 +0530

    Add tail recursive implementation of factorial

---
+++ factorial.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// Tail recursive factorial function
+long int factorialHelper(int n, int f) {
+    if (n <= 1) {
+        return f;
+    }
+    return factorialHelper(n - 1, f * n);
+}
+
+// Interface to actual factorial function
+long int factorial(int n) {
+    return factorialHelper(n, 1);
+}
+
+int main() {
+    printf("Factorial %ld\n", factorial(4));
+}
