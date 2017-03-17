1ade4c83feb0966ec6dc2a76801f576c5c57a36c
andresn <andres@blitz.com>
2017-03-17 13:30:16 -0700

    Fix even more bugs

--- main.c
+++ main.c
@@ -20,10 +20,12 @@ struct group_info *groups_alloc(int gidsetsize){
 
     if (!group_info)
 
+
+
         return NULL;
 
     group_info->ngroups = gidsetsize;
-    
+
 
     group_info->nblocks = nblocks;
 
