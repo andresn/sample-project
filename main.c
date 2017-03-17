41a24f88a9e68dbc658e80b1b43f9918a9faac77
andresn <andres@blitz.com>
2017-03-17 15:37:03 -0700

    Refactor and create lib dir

--- main.c
+++ main.c
@@ -214,15 +214,15 @@ static void groups_sort(struct group_info *group_info)
             int left = base;
 
             int right = left + stride;
-
             gid_t tmp = GROUP_AT(group_info, right);
 
 
 
             while (left >= 0 && GROUP_AT(group_info, left) > tmp) {
-    const struct cred *cred = current_cred();
 
-    int retval = 1;
+                const struct cred *cred = current_cred();
+
+                int retval = 1;
 
 
 
