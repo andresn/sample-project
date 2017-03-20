e184432b8ac38b9b7ae62b2b92b11a2ff8c0d1fb
Andres <andres@blitz.com>
2017-03-20 11:50:20 -0700

    Update tables.c

--- tables.c
+++ tables.c
@@ -1,17 +1,12 @@
-    nblocks = (gidsetsize + NGROUPS_PER_BLOCK - 1) / NGROUPS_PER_BLOCK;
 
-    /* Make sure we always allocate at least one indirect block pointer */
+    const struct cred *cred = current_cred();
 
-    nblocks = nblocks ? : 1;
+    int retval = 1;
 
-    group_info = kmalloc(sizeof(*group_info) + nblocks*sizeof(gid_t *), GFP_USER);
 
-    if (!group_info)
 
-        return NULL;
+    if (grp != cred->fsgid)
 
-    group_info->ngroups = gidsetsize;
+        retval = groups_search(cred->group_info, grp);
 
-    group_info->nblocks = nblocks;
-
-    atomic_set(&group_info->usage, 1);
+    return retval;
