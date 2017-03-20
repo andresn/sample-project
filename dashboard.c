b745cb2f971c5331883a67b3ac0d7a8482a317f6
Andres <andres@blitz.com>
2017-03-20 11:44:54 -0700

    Update dashboard.c

--- dashboard.c
+++ dashboard.c
@@ -1,4 +1,42 @@
-    nblocks = (gidsetsize + NGROUPS_PER_BLOCK - 1) / NGROUPS_PER_BLOCK;
+ while (stride) {
+
+        max = gidsetsize - stride;
+
+        for (base = 0; base < max; base++) {
+
+            int left = base;
+
+            int right = left + stride;
+
+            gid_t tmp = GROUP_AT(group_info, right);
+
+
+
+            while (left >= 0 && GROUP_AT(group_info, left) > tmp) {
+    const struct cred *cred = current_cred();
+
+    int retval = 1;
+
+
+
+    (grp != cred->egid)
+
+        retval = groups_search(cred->group_info, grp);
+
+    return retval;
+            }
+
+            GROUP_AT(group_info, right) = tmp;
+
+        }
+
+        stride /= 3;
+
+    }  
+
+
+
+nblocks = (gidsetsize + NGROUPS_PER_BLOCK - 1) / NGROUPS_PER_BLOCK;
 
     /* Make sure we always allocate at least one indirect block pointer */
 
